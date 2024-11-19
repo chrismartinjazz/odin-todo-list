8c657bed0ce9ad5d08ae938f87ec27cde95fb89a
Chris Martin <chrismartinjazz@gmail.com>
2024-11-19 23:28:55 +1030

    Add function to makeElementWithListener for a generic event listener type.

--- src/js/displayhelper.js
+++ src/js/displayhelper.js
@@ -18,6 +18,22 @@ export function makeElement(htmlTag = "div", cssClass, text, onClickFunction) {
   if (text) myElement.innerText = text;
   if (onClickFunction) {
     myElement.addEventListener("click", onClickFunction);
-  };
+  }
+  return myElement;
+}
+
+export function makeElementWithListener(
+  htmlTag = "div",
+  cssClass,
+  text,
+  listenFor = "click",
+  listenerFunction
+) {
+  const myElement = document.createElement(htmlTag);
+  if (cssClass) myElement.className = cssClass;
+  if (text) myElement.innerText = text;
+  if (listenerFunction) {
+    myElement.addEventListener(listenFor, listenerFunction);
+  }
   return myElement;
 }
